399540160857edd543283b5a24f9a2631fb94b52
chan myae aung <chanmyaeaung@better.hr>
2019-08-04 20:11:34 +0630

    add detail screen

--- app/src/main/java/io/material/rally/ui/detail/MyPagerAdapter.kt
+++ app/src/main/java/io/material/rally/ui/detail/MyPagerAdapter.kt
@@ -3,14 +3,13 @@ package io.material.rally.ui.detail
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import io.material.rally.ui.account.AccountFragment
 
 /**
  * Created by Chan Myae Aung on 8/4/19.
  */
 class MyPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
   override fun getItem(position: Int): Fragment {
-    return AccountFragment()
+    return DetailFragment()
   }
 
   override fun getCount(): Int {
