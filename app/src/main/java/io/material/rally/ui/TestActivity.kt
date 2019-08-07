d77ad1cff51bb87cb0c5e02f30ce504bddcf208b
Lin Min Phyo <linminphyo@nexlabs.co>
2019-08-07 22:20:47 +0630

    Update typography of Subtitle1 letter spacing and add tablet support for the first time

---
+++ app/src/main/java/io/material/rally/ui/TestActivity.kt
@@ -0,0 +1,16 @@
+package io.material.rally.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.material.rally.R
+
+/**
+ * Created by lin min phyo on 2019-08-06.
+ */
+
+class TestActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.test)
+  }
+}
